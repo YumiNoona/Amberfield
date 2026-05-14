adb5f9d7a58834ac6ab48d5c18ee7a4d6dd417c7
Yuna <rushikeshingale2001@gmail.com>
2026-05-14 18:29:59 +0530

    Added Skill Icons

--- Scripts/UI/SkillButton.gd
+++ Scripts/UI/SkillButton.gd
@@ -1,2 +1,28 @@
 extends Button
 class_name SkillButton
+
+@export var is_free: bool
+@export var skill_data: SkillData
+
+@onready var skill_icon: TextureRect = $SkillIcon
+@onready var lock: TextureRect = $Lock
+
+var skill: SkillData
+var is_unlocked: bool
+
+func _ready() -> void:
+	enable_skill(false)
+	
+	if skill_data:
+		load_data(skill)
+	if is_free:
+		enable_skill(true)
+
+func load_data(data: SkillData) -> void:
+	skill = data
+	skill_icon.texture = data.icon
+
+
+func enable_skill(value: bool) -> void:
+	skill_icon.self_modulate = (Color.WHITE if value else Color("787878"))
+	lock.visible = not value
